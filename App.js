26d49b2c5c41a74769de0527e006700510494c65
chihyeonWON <heungeob1003@gamil.com>
2021-11-26 14:42:39 +0900

    Change useState importing in react-native -> react

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, useState } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { theme } from './colors';
 
 export default function App() { 
