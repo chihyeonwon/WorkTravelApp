63ff1a7f40b1df76c29ba2fa06b0f058f08afe1e
chihyeonWON <heungeob1003@gamil.com>
2021-11-26 14:35:57 +0900

    work일때와 travel 일때의 상태를 저장하는 state를 생성

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { theme } from './colors';
 
-export default function App() {
+export default function App() { 
+  const [working, setWorking] = useState(true); // work일때 상태를 저장하는 useState 함수 생성
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);  
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
